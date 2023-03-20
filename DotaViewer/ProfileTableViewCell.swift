800dfd5fe8b1c8e1be6a31c78f8596e3bed7ef46
Stepan <ifomstep@icloud.com>
2023-03-20 20:52:21 +0500

    Add custom cell

---
+++ DotaViewer/ProfileTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileTableViewCell.swift
+//  DotaViewer
+//
+//  Created by Степан Фоминцев on 20.03.2023.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+    @IBOutlet weak var profileNameLabel: UILabel!
+}
