9e225b168f37b5aee344a3b467e26e945643f090
Stepan <ifomstep@icloud.com>
2023-03-20 20:51:54 +0500

    Add logic to search

---
+++ DotaViewer/SearchViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  DotaViewer
+//
+//  Created by Степан Фоминцев on 20.03.2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var profiles: Profile = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+    }
+
+
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        fetchProfiles()
+    }
+    
+    private func fetchProfiles() {
+        guard textField.text != nil else { return }
+        let urlString = "https://api.opendota.com/api/search?q=\(textField.text!)"
+        guard let url = URL(string: urlString) else { return }
+        var request = URLRequest(url: url, timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data else { return }
+            guard error == nil else { return }
+            let profilesData = try? JSONDecoder().decode(Profile.self, from: data)
+            guard let profilesData else { return }
+            DispatchQueue.main.async {
+                self.profiles = []
+                for profile in profilesData {
+                    self.profiles.append(profile)
+                }
+                print(self.profiles)
+                self.tableView.reloadData()
+            }
+        }.resume()
+    }
+}
+
