607ca3e9a13c7ad6407c8d4851c2b9dc9abc1552
Stepan <ifomstep@icloud.com>
2023-03-21 01:37:02 +0500

    Add model for detailed info

---
+++ DotaViewer/DetailedProfileModel.swift
@@ -0,0 +1,12 @@
+//
+//  DetailedProfileModel.swift
+//  DotaViewer
+//
+//  Created by Степан Фоминцев on 21.03.2023.
+//
+
+import Foundation
+
+struct DetailedProfile: Codable {
+    let win, lose: Int
+}
