949fb4e5f6a007a8387150d168826c85a3747505
Stepan <ifomstep@icloud.com>
2023-03-21 15:51:36 +0500

    Add featured hero custom cell

---
+++ DotaViewer/FeaturedHeroTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeaturedHeroTableViewCell.swift
+//  DotaViewer
+//
+//  Created by Степан Фоминцев on 21.03.2023.
+//
+
+import UIKit
+
+class FeaturedHeroTableViewCell: UITableViewCell {
+    @IBOutlet weak var heroNameLabel: UILabel!
+    @IBOutlet weak var totalGamesLabel: UILabel!
+    @IBOutlet weak var totalWinsLabel: UILabel!
+    @IBOutlet weak var winRateLabel: UILabel!
+}
